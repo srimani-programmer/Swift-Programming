643ff6db7d578ab814092ee64091cdbe44eae0d0
srimani-programmer <srimani.scholar@gmail.com>
2019-02-11 17:25:04 +0530

    Operations on Variables.

---
+++ Variables/variableOperations.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var val1 = 10
+
+var val2 = 20
+
+// Addition of Two Numbers
+
+print("Addition Results:")
+print("Addition of Two number is: \(val1 + val2)")
+
+// Subtraction of two numbers
+
+print("Subtraction Results:")
+print("Subtraction of two numbers is: \(val2 - val1)")
+
+// Multiplication of two numbers
+
+print("Multiplication Results:")
+print("Multiplication of two numbers is: \(val2 * val1)")
+
+// Division of two numbers
+
+print("Division Results:")
+print("Division of two numbers is: \(val2 / val1)")
+print("Division of two numbers is: \(val1 / val2)")
+
+// Modulus of two numbers
+
+print("Modulus Results:")
+print("Modulus of two numbers is: \(val2 % val1)")
+print("Modulus of two numbers is: \(val1 % val2)")
+
+
+
+
+
+
+
+
