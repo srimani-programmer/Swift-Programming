a6dfa279b6cca46cb9fc7775e1c9bd764bd975dd
srimani-programmer <srimani.scholar@gmail.com>
2019-02-11 18:41:21 +0530

    Nested Conditional Statements.

--- Conditional Statements/NestedIFPlayground.playground/Contents.swift
+++ Conditional Statements/NestedIFPlayground.playground/Contents.swift
@@ -7,15 +7,15 @@ var percentage : Float = 91.245
 if(percentage >= 90 && percentage <= 100){
     print("Outstanding Performace.")
 }else if(percentage >= 80 && percentage < 90){
-    print("A+ Grade")
+    print("Grade: A+")
 }else if(percentage >= 70 && percentage < 80){
-    print("A Grade")
+    print("Grade: A")
 }else if(percentage >= 60 && percentage < 70){
-    print("B+ Grade")
+    print("Grade: B+")
 }else if(percentage >= 50 && percentage < 60){
-    print("B Grade")
+    print("Grade: B")
 }else if(percentage >= 40 && percentage < 50){
-    print("C Grade")
+    print("Grade: C")
 }else{
     print("You're Fail")
 }
