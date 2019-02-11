46bcbebb57046b903ddf02ff3e9e6662879eb4d2
srimani-programmer <srimani.scholar@gmail.com>
2019-02-11 17:49:02 +0530

    Datatypes Introduction.

---
+++ Datatypes/datatype.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// Declaring a variable particularly with specific datatype
+var myName : String = "Sri Manikanta Palakollu"
+
+var myage : Int = 19
+
+var mySalary : Float = 2500000
+
+var cgpa : Float = 9.5888
+
+var collegeName : String = "Sri indu College of Engineering and Technology."
+
+// Printing the results
+
+print("Name: \(myName)")
+print("Age: \(myage)")
+print("My Salary: \(mySalary)")
+print("CGPA : \(cgpa)")
+print("College Name: \(collegeName)")
+
