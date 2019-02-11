f0c746381d9bcf478d143cb2aadb9fd7da7dd5a3
srimani-programmer <srimani.scholar@gmail.com>
2019-02-11 19:09:45 +0530

    Switch Statements.

---
+++ Conditional Statements/switchPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+// Switch Statements
+
+import UIKit
+
+var myFavouriteColor : String = "gold"
+switch myFavouriteColor {
+case "red":
+    print("You Got 100 Points.")
+case "blue":
+    print("You Got 90 Points.")
+case "orange":
+    print("You Got 80 Points.")
+case "silver":
+    print("You Got 70 Points.")
+case "gold":
+    print("You got 60 Points.")
+case "pink":
+    print("You got 50 Points.")
+default:
+    print("You Got 40 Points")
+}
