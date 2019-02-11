46244734fa17e69c6ae14248a2b5a509204c14aa
srimani-programmer <srimani.scholar@gmail.com>
2019-02-11 20:07:11 +0530

    Function concept in swift.

---
+++ Functions/MyFirstFunctionPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+// Playing with functions
+
+import UIKit
+// Addition Function
+func addTwoNumbers(){   // Function declaration or called Function
+    let a = 15
+    let b = 22
+    print("Addition of Two Numbers: \(a + b)")
+}
+
+// Subtraction Function
+
+func subtractTwoNumbers(){
+    let a = 22
+    let b = 15
+    print("Subtraction of two numbers: \(a - b)")
+    
+}
+
+// Function calls or function calls
+
+addTwoNumbers()
+subtractTwoNumbers()
