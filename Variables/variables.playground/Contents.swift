4a377f430cb35dbd6d783953c5d2e33572edf385
srimani-programmer <srimani.scholar@gmail.com>
2019-02-11 17:16:39 +0530

    Variables Introduction.

---
+++ Variables/variables.playground/Contents.swift
@@ -0,0 +1,23 @@
+// Variables in Swift.
+// copyrighted by @Sri Manikanta Palakollu
+// Apple.Inc 2019
+
+import UIKit
+
+var myName = "Sri Manikanta palakollu."
+
+var age = 19
+
+var salary = 2500000
+
+let girlFriendCount = 0
+
+print("My name is: \(myName)")
+
+print("My Age: \(age)")
+
+print("My Salary: \(salary)")
+
+print("Girl Friend Count: \(girlFriendCount)")
+
+
